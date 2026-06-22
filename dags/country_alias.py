78577292525f5ced91e048b48b238dbecbda2c09
Scastnijs <andrejsscastnijs@gmail.com>
2026-06-23 00:15:22 +0300

    country alias

---
+++ dags/country_alias.py
@@ -0,0 +1,59 @@
+from datetime import datetime
+
+from airflow import DAG
+from airflow.decorators import task
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+
+
+with DAG(
+    dag_id="country_alias",
+    start_date=datetime(2025, 1, 1),
+    schedule=None,
+    catchup=False,
+) as dag:
+
+    @task
+    def read_csv():
+        import csv
+
+        csv_file = "/opt/airflow/data/country_alias.csv"
+
+        rows = []
+
+        with open(csv_file, "r", encoding="utf-8") as f:
+            reader = csv.DictReader(f)
+
+            for row in reader:
+                rows.append(
+                    (
+                        row["alias_country"],
+                        row["canonical_country"]
+                    )
+                )
+
+        return rows
+
+    @task
+    def load_to_postgres(rows):
+        hook = PostgresHook(postgres_conn_id="postgres")
+
+        insert_sql = """
+        INSERT INTO staging.country_alias
+        (alias_country, canonical_country)
+        VALUES (%s, %s)
+        """
+
+        conn = hook.get_conn()
+        cursor = conn.cursor()
+
+        for row in rows:
+            cursor.execute(insert_sql, row)
+
+        conn.commit()
+
+        cursor.close()
+        conn.close()
+
+    data = read_csv()
+
+    load_to_postgres(data)
--- database/postgres_init.sql
+++ database/postgres_init.sql
@@ -3,61 +3,67 @@ create schema if not exists staging;
 
 create table if not exists staging.drinks
 (
-id int generated always as identity primary key,
-country varchar(60) not null,
-beer int,
-spirit int,
-wine int,
-total_litres decimal(10,2),
-continent char(2)
+	id int generated always as identity primary key,
+	country varchar(60) not null,
+	beer int,
+	spirit int,
+	wine int,
+	total_litres decimal(10,2),
+	continent char(2)
 );
 
 create table if not exists staging.expectancy
 (
-id int generated always as identity primary key,
-country varchar(60) not null,
-both_g decimal(10,2),
-female decimal(10,2),
-male decimal(10,2)
+	id int generated always as identity primary key,
+	country varchar(60) not null,
+	both_g decimal(10,2),
+	female decimal(10,2),
+	male decimal(10,2)
 );
 
 create table if not exists staging.worldcities 
 (
-id int generated always as identity primary key,
-city varchar(60),
-city_ascii varchar(60),
-lat decimal(7,4),
-lng decimal(7,4),
-country varchar(50),
-iso2 char(2),
-iso3 char(3),
-admin_name varchar(100),
-capital varchar(10),
-population int
+	id int generated always as identity primary key,
+	city varchar(60),
+	city_ascii varchar(60),
+	lat decimal(7,4),
+	lng decimal(7,4),
+	country varchar(60),
+	iso2 char(2),
+	iso3 char(3),
+	admin_name varchar(100),
+	capital varchar(10),
+	population int
 );
 
 create table if not exists staging.vehicles_country
 (
-id int generated always as identity primary key,
-country varchar(60) not null,
-per1kpeople int,
-total int,
-year int
+	id int generated always as identity primary key,
+	country varchar(60) not null,
+	per1kpeople int,
+	total int,
+	year int
 );
 
 create table if not exists staging.happiness
 (
-id int generated always as identity primary key,
-year int,
-country varchar(60),
-region varchar(60),
-rank int,
-score decimal(10,5),
-gdp_per_capita decimal(7,5),
-family decimal(7,5),
-health decimal(7,5),
-freedom decimal(7,5),
-trust decimal(7,5),
-generosity decimal(7,5),
-dystopia decimal(7,5)
+	id int generated always as identity primary key,
+	year int,
+	country varchar(60),
+	region varchar(60),
+	rank int,
+	score decimal(10,5),
+	gdp_per_capita decimal(7,5),
+	family decimal(7,5),
+	health decimal(7,5),
+	freedom decimal(7,5),
+	trust decimal(7,5),
+	generosity decimal(7,5),
+	dystopia decimal(7,5)
+);
+
+CREATE TABLE staging.country_alias 
+(
+    alias_country varchar(100) PRIMARY KEY,
+    canonical_country varchar(60) NOT NULL
 );
