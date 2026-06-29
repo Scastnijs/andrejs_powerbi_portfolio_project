ed70d69fd7ac3a188ec4d3fd5a4cd0b871ba3a54
Scastnijs <andrejsscastnijs@gmail.com>
2026-06-29 17:43:13 +0300

    dw_drinks

--- dags/dw_drinks.py
+++ dags/dw_drinks.py
@@ -17,15 +17,17 @@ def task1(**context):
     hook = PostgresHook(postgres_conn_id="postgres")
 
     records = hook.get_records("""
-        SELECT      
-        dc.cust_first_name, 
-        dc.cust_last_name,
-        dc.cust_city, 
-        ds.state_name,
-        dc.cust_state 
-        
-        FROM WSH.DEMO_CUSTOMERS dc
-        LEFT JOIN WSH.DEMO_STATES ds ON dc.CUST_STATE = ds.ST
+        SELECT
+            c.iso2,
+            COALESCE(ca.canonical_country, d.country) AS country,
+            c.iso2,
+            d.continent        
+        FROM staging.drinks d
+        LEFT JOIN staging.country_alias ca
+            ON d.country = ca.alias_country
+        left join staging.countries c
+            on COALESCE(ca.canonical_country, d.country) = c.name_short
+        where c.iso2 is not null
     """)
 
     # Push data to XCom
@@ -44,33 +46,33 @@ def task2(**context):
     hook = MySqlHook(mysql_conn_id="mysql")
 
     insert_sql = """
-        INSERT INTO customer_location 
+        INSERT INTO dw.dim_geo 
             (
-                CUST_FIRST_NAME, 
-                CUST_LAST_NAME, 
-                CUST_COUNTRY,
-                CUST_COUNTRY_CODE,
-                CUST_CITY,
-                CUST_STATE,
-                CUST_STATE_CODE
+                geo_code, 
+                geo_name, 
+                geo_type,
+                iso2,
+                continent_code
             ) 
             VALUES 
             (
                 %s,
                 %s,
-                'United States',
-                'US',
-                %s,
+                'country',
                 %s,
                 %s
             )
+            ON DUPLICATE KEY UPDATE
+            geo_name = VALUES(geo_name),
+            iso2 = VALUES(iso2),
+            continent_code = VALUES(continent_code)
     """
 
     for row in records:
         hook.run(insert_sql, parameters=row)
 
 with DAG(
-    dag_id='map_load1',
+    dag_id='dw_drinks',
     default_args=default_args,
     start_date=datetime(2026, 1, 20),
     schedule='0 0 * * *'
